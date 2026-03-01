9b945a7053b556c8dd59299517b15ca57ef1f923
Josué Tapia Hernández <josuetapiahernandez@gmail.com>
2026-02-28 23:10:49 -0600

    chore: Add test for mongodb credentials

---
+++ tools/tests/tools/test_mongodb.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+uri = os.getenv("MONGODB_URI")
+
+client = MongoClient(uri)
+
+try:
+    client.admin.command('ping')
+    print("¡Success!")
+except Exception as e:
+    print(e)
